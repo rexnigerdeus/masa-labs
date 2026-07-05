27b61cc1dacacd9fc4be8271cefc643979948feb
Daniel Tossou <tossdaniel@outlook.com>
2026-07-05 02:01:39 +0000

    fix: switch to lucide_icons_flutter (compatible Dart 3)
    - Old lucide_icons 0.257.0 was incompatible with Dart 3.8 / Flutter 3.32
    - Switched to lucide_icons_flutter 3.1.14+2 (actively maintained)
    - Updated all imports from lucide_icons to lucide_icons_flutter
    - flutter clean + pub get to reload fonts
    - Icons should now render correctly on web

--- rondo/lib/features/auth/screens/login_screen.dart
+++ rondo/lib/features/auth/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import '../../../core/theme/app_theme.dart';
 import '../../../core/providers.dart';
